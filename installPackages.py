1ea9549ee3303809e2c11002af9e37a6db8674d3
2press <pres.sure@ymail.com>
2017-09-04 20:59:47 +0200

    Clean up

--- installPackages.py
+++ installPackages.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
+"""Install Packages for Starcraft Casting Tool."""
 import pip
-import sys
 import platform
 import logging
 
@@ -15,25 +14,26 @@ fh = logging.FileHandler('src/installpackages.log', 'w')
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
 logger.addHandler(ch)
 
-logger.info("system: "+system)
- 
+logger.info("system: " + system)
+
 try:
     pip.main(['install', 'PyQt5'])
     pip.main(['install', 'requests'])
     pip.main(['install', 'configparser'])
     pip.main(['install', 'flask'])
-    pip.main(['install','obs-ws-rc'])
-    pip.main(['install','pytesseract'])
-    pip.main(['install','Pillow'])
+    pip.main(['install', 'obs-ws-rc'])
+    pip.main(['install', 'pytesseract'])
+    pip.main(['install', 'Pillow'])
 
-    if(system=="Windows"):
+    if(system == "Windows"):
         pip.main(['install', 'pypiwin32'])
 
 except Exception as e:
