58d450d2d4ea8e91d637e1cd35612e42c7855163
2press <pres.sure@ymail.com>
2017-09-07 22:42:17 +0200

    Icon

--- scctool/__init__.py
+++ scctool/__init__.py
@@ -10,14 +10,19 @@ def main():
     """Run Starcraft Casting Tool."""
     from PyQt5.QtWidgets import QApplication, QStyleFactory
     from PyQt5.QtGui import QIcon
+    from PyQt5.QtCore import QSize
     from scctool.view.main import MainWindow
     from scctool.controller import MainController
+    from scctool.settings import getAbsPath
 
     try:
         """Run the main program."""
         app = QApplication(sys.argv)
         QApplication.setStyle(QStyleFactory.create('Fusion'))
-        app.setWindowIcon(QIcon('src/icon.png'))
+        icon = QIcon()
+        icon.addFile(getAbsPath('src/scct.ico'),QSize(32, 32))
+        icon.addFile(getAbsPath('src/scct.png'),QSize(256, 256))
+        app.setWindowIcon(icon)
         cntlr = MainController()
         MainWindow(cntlr, app)
         logger.info("Starting...")
