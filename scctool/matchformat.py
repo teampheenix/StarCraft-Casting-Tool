087b7126154400abae56e8b14609d79803fe2315
2press <pres.sure@ymail.com>
2018-04-22 11:08:08 +0200

    Match format fix.

--- scctool/matchformat.py
+++ scctool/matchformat.py
@@ -74,3 +74,57 @@ class MatchFormatPsi(MatchFormat):
         self._matchData.setURL(self._url)
         self._matchData.setLeague(self._name)
         self._matchData.writeJsonFile()
+
+
+class MatchFormatAllInTheNydus(MatchFormat):
+    """Interface definition"""
+
+    _name = "All-In TheNydus"
+
+    def __init__(self, *args):
+        """Init match grabber."""
+        super().__init__(*args)
+        self._url = "https://sites.google.com/site/allinthenydus"
+
+    def applyFormat(self):
+        self._matchData.setCustom(7, True, False)
+        self._matchData.setMinSets(4)
+        self._matchData.setURL(self._url)
+        self._matchData.setLeague(self._name)
+        self._matchData.writeJsonFile()
+
+
+class MatchFormatVSL(MatchFormat):
+    """Interface definition"""
+
+    _name = "Validity Star League"
+
+    def __init__(self, *args):
+        """Init match grabber."""
+        super().__init__(*args)
+        self._url = "http://liquipedia.net/starcraft2/Validity_Star_League"
+
+    def applyFormat(self):
+        self._matchData.setCustom(7, True, False)
+        self._matchData.setMinSets(4)
+        self._matchData.setURL(self._url)
+        self._matchData.setLeague(self._name)
+        self._matchData.writeJsonFile()
+
+
+class MatchFormatChobo(MatchFormat):
+    """Interface definition"""
+
+    _name = "Chobo Team League"
+
+    def __init__(self, *args):
+        """Init match grabber."""
+        super().__init__(*args)
+        self._url = "http://www.choboteamleague.com"
+
+    def applyFormat(self):
+        self._matchData.setCustom(8, False, False)
+        self._matchData.setMinSets(8)
+        self._matchData.setURL(self._url)
+        self._matchData.setLeague(self._name)
+        self._matchData.writeJsonFile()
