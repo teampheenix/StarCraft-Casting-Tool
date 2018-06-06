807b60105255d6834f804164e2ba2c0feef000cc
2press <pres.sure@ymail.com>
2018-06-06 22:06:59 +0200

    Version number

--- scctool/__init__.py
+++ scctool/__init__.py
@@ -12,7 +12,7 @@ import scctool.settings.config
 
 logger = logging.getLogger('scctool')
 
-__version__ = "2.0.0beta11"
+__version__ = "2.0.0beta12"
 __latest_version__ = __version__
 __new_version__ = False
 
