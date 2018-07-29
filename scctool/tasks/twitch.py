493a04f46ecc8ace561bf2f421e4c52fbe32638a
2press <pres.sure@ymail.com>
2018-07-29 14:27:27 +0200

    Chg: Upgrade to Twitch-API v5.

--- scctool/tasks/twitch.py
+++ scctool/tasks/twitch.py
@@ -19,15 +19,19 @@ def updateTitle(newTitle):
     try:
         twitchChannel = scctool.settings.config.parser.get(
             "Twitch", "Channel").strip()
+        userID = getUserID(twitchChannel)
+
         clientID = TWITCH_CLIENT_ID
         oauth = scctool.settings.config.parser.get("Twitch", "oauth")
 
-        headers = {'Accept': 'application/vnd.twitchtv.v3+json',
+        headers = {'Accept': 'application/vnd.twitchtv.v5+json',
                    'Authorization': 'OAuth ' + oauth,
                    'Client-ID': clientID}
-        params = {'channel[status]': newTitle, 'channel[game]': 'StarCraft II'}
 
-        requests.put('https://api.twitch.tv/kraken/channels/' + twitchChannel,
+        params = {'channel[status]': newTitle}
+        params['channel[game]'] = 'StarCraft II'
+
+        requests.put('https://api.twitch.tv/kraken/channels/' + userID,
                      headers=headers, params=params).raise_for_status()
         msg = _('Updated Twitch title of {} to: "{}"').format(
             twitchChannel, newTitle)
@@ -38,7 +42,8 @@ def updateTitle(newTitle):
         status_code = e.response.status_code
         error_msg = "Twitch API-Error: {}"
         if(status_code == 404):
-            msg = _("Not Found - Channel '{}' not found.").format(twitchChannel)
+            msg = _("Not Found - Channel '{}'"
+                    " not found.").format(twitchChannel)
             msg = error_msg.format(msg)
         elif(status_code == 403):
             msg = error_msg.format(_("Forbidden - Do you have permission?"))
@@ -56,3 +61,17 @@ def updateTitle(newTitle):
         module_logger.exception("message")
 
     return msg, success
+
+
+def getUserID(user):
+
+    clientID = TWITCH_CLIENT_ID
+    headers = {'Accept': 'application/vnd.twitchtv.v5+json',
+               'Client-ID': clientID}
+    params = {'login': user}
+
+    response = requests.get('https://api.twitch.tv/kraken/users',
+                            headers=headers, params=params)
+    response.raise_for_status()
+    data = response.json()
+    return data['users'][0]['_id']
