97e5407e44c62952d5d8e65e933779663693107e
2press <pres.sure@ymail.com>
2018-09-10 17:53:12 +0200

    Fix: Fixed a bug where the match format would not update when changing the match tabs.

--- scctool/view/main.py
+++ scctool/view/main.py
@@ -540,8 +540,8 @@ class MainWindow(QMainWindow):
         dataWidget = self.matchDataTabWidget.widget(idx)
         ident = dataWidget.matchData.getControlID()
         self.controller.matchControl.selectMatch(ident)
-        # with self.tlock:
-        #    self.controller.updateMatchFormat()
+        with self.tlock:
+            self.controller.updateMatchFormat()
 
     def tabMoved(self, toIdx, fromIdx):
         self.controller.matchControl.updateOrder(toIdx, fromIdx)
