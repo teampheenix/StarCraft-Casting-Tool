03550728cea944ff520fb70f24c5bbe7312927ca
2press <pres.sure@ymail.com>
2019-04-27 13:41:53 +0200

    New: Added `ticker.txt` to `casting_data`. This file contains all non-zero score matches defined by your open tabs in the form `Team A 1-2 TeamB | TeamC 1-0 TeamD | ` to be readily included as a ticker for all latest results.

--- scctool/tasks/textfiles.py
+++ scctool/tasks/textfiles.py
@@ -24,9 +24,10 @@ class TextFilesThread(TasksThread):
         self._q = SetQueue()
         self._available_items = ['team', 'score',
                                  'meta', 'league', 'bestof',
-                                 'cd_start', 'cd_end']
+                                 'cd_start', 'cd_end', 'ticker']
         self._matchControl.dataChanged.connect(self.put)
         self._matchControl.metaChanged.connect(self.put)
+        self._matchControl.tickerChanged.connect(lambda: self.put('ticker'))
         self.addTask('write', self.__writeTask)
         self.activateTask('write')
         self.put('meta')
@@ -51,11 +52,14 @@ class TextFilesThread(TasksThread):
                 self.__writeCountdown(True)
             elif item == "cd_end":
                 self.__writeCountdown(False)
+            elif item == "ticker":
+                self.__writeTicker()
             else:
                 self.__writeBestOf()
                 self.__writeTeam()
                 self.__writeScore()
                 self.__writeLeague()
+                self.__writeTicker()
         except queue.Empty:
             pass
         finally:
@@ -71,6 +75,13 @@ class TextFilesThread(TasksThread):
             txt = scctool.settings.config.parser.get("Countdown", "post_txt")
         with open(file, mode='w', encoding='utf-8') as f:
             f.write(txt)
+
+    def __writeTicker(self):
+        file = scctool.settings.getAbsPath(
+            scctool.settings.casting_data_dir
+            + "/ticker.txt")
+        with open(file, mode='w', encoding='utf-8') as f:
+            f.write(self._matchControl.getTickerText())
 
     def __writeTeam(self):
         file = scctool.settings.getAbsPath(
