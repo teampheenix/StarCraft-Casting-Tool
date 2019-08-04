33c3788329572476824d37eb9a540eb99f8779c7
2press <pres.sure@ymail.com>
2019-08-04 19:19:41 +0200

    Version number increased.

--- scctool/__init__.py
+++ scctool/__init__.py
@@ -11,7 +11,7 @@ import scctool.settings.translation
 
 logger = logging.getLogger(__name__)
 
-__version__ = "2.7.9"
+__version__ = "2.7.10"
 __latest_version__ = __version__
 __new_version__ = False
 
