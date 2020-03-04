ea4256b607e9b85d2fe4dace5f99ff2935d58f12
2press <pres.sure@ymail.com>
2020-03-04 17:55:47 +0100

    Requirements updated

--- StarCraftCastingTool.py
+++ StarCraftCastingTool.py
@@ -9,12 +9,17 @@ logger.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
 fh = logging.FileHandler(scctool.settings.getLogFile(), 'w')
 # create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create console handler with a higher log level
 # create formatter and add it to the handlers
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
+ch.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
+logger.addHandler(ch)
 
 if __name__ == '__main__':
     try:
