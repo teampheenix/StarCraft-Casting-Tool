e28ee9c1d365bc7fe8894d3fa2019dfa321ac207
2press <pres.sure@ymail.com>
2020-11-13 19:03:22 +0100

    Test fix

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 from scctool.tasks.aligulac import AligulacInterface
 from scctool.tasks.liquipedia import LiquipediaGrabber
 
+
 def test_aligulac(aligulac_api_key):
     aligulac = AligulacInterface(aligulac_api_key)
     player1 = aligulac.search_player('pressure', 'P')
@@ -8,24 +9,26 @@ def test_aligulac(aligulac_api_key):
     assert player1['tag'] == 'pressure'
     assert player1['race'] == 'P'
     assert player1['country'] == 'DE'
-    #TODO: fix this
+    # TODO: fix this
     #player2 = aligulac.get_player(player1)
     #assert player2['id'] == player1['id']
     player2 = aligulac.get_player(player1['id'])
     assert player2['id'] == player1['id']
-    
-    prediction = aligulac.predict_match('Maru', 'Serral', bo=5, score1=0, score2=0)
+
+    prediction = aligulac.predict_match(
+        'Clem', 'Serral', bo=5, score1=0, score2=0)
     assert prediction is not None
     assert len(prediction['outcomes']) == 6
-    assert prediction['pla']['id'] == 49
+    assert prediction['pla']['id'] == 5878
     assert prediction['plb']['id'] == 485
     score = aligulac.predict_score(prediction)
     assert score is not None
 
-    history = aligulac.get_history('Maru', 'Serral')
+    history = aligulac.get_history('Clem', 'Serral')
     assert history is not None
     assert len(history['objects']) > 0
-    
+
+
 def test_liquipedia():
     grabber = LiquipediaGrabber()
     mappool = list(grabber.get_ladder_mappool())
